762c2a197d12a35ed2aeae31911022ad6cf71139
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-28 06:45:24 -0400

    TDD Basics with C# -Example FizzBuzz-initial commit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,13 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for(int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
