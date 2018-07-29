33fcaa03b68a914255df37d171c3968f61d42a16
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-29 08:03:47 -0400

    FizzBuzzer.cs is created in FizzBuzz.Library in which the conditions to pass all the tests are coded

---
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Library
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int input)
+        {
+            if (input % 3 == 0)
+                return "Fizz";
+            if (input % 5 == 0)
+                return "Buzz";
+            return input.ToString();
+        }
+    }
+}
