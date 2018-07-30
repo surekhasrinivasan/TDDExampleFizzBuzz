f9c4086b9705a8ce57cac3c06c16f13b3a1d243c
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-30 06:21:58 -0400

    FizzBuzzer.cs modified

--- FizzBuzz.Library/FizzBuzzer.cs
+++ FizzBuzz.Library/FizzBuzzer.cs
@@ -10,11 +10,17 @@ namespace FizzBuzz.Library
     {
         public static string GetValue(int input)
         {
+            string output = string.Empty;
+
+            //if (input % 3 == 0 && input % 5 == 0)
+            //    return "FizzBuzz";
             if (input % 3 == 0)
-                return "Fizz";
+                output += "Fizz";
             if (input % 5 == 0)
-                return "Buzz";
-            return input.ToString();
+                output += "Buzz";
+            if(string.IsNullOrEmpty(output))
+                output = input.ToString();
+            return output;
         }
     }
 }
