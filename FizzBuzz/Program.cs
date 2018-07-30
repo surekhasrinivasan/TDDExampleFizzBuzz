802dd31a0b2ce66f786f5ce20380264df8b3d801
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-30 06:26:22 -0400

    Program.cs modified, prints Fizz,Buzz and FizzBuzz for numbers divisible by 3, 5 and both respectively on Console

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FizzBuzz.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace FizzBuzz
         {
             for(int i = 1; i <= 100; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(FizzBuzzer.GetValue(i));
             }
 
             Console.ReadLine();
